cb2e062241733d2890da1972f9bf2e9d960e3874
kevin ponce <kevin.ponce@ieesa.edu.hn>
2025-07-15 21:17:25 +0200

    correcion en el compose  y en vue configs

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',        // Permite conexiones externas
+    port: 3000,             // Asegúrate que coincide con el puerto expuesto
+    allowedHosts: 'all'     // Acepta cualquier cabecera 'Host' (como tu dominio)
+  }
 })
